eac179777e287d2f6260797e8a8e0b239c77dd77
Hossori <law.f1.vegas@gmail.com>
2023-07-11 21:26:48 +0900

    a

--- app/src/ts/constant/path/pathConst.ts
+++ app/src/ts/constant/path/pathConst.ts
@@ -1,5 +1,5 @@
-class PathConst {
-    static HTML_ROOT = "app/dist/resources/html/";
-    static HTML_MAIN:string = PathConst.HTML_ROOT + "main";
-    static HTML_TAB:string = PathConst.HTML_ROOT + "tab";
+export default class PathConst {
+    HTML_ROOT = "app/dist/resources/html/";
+    HTML_MAIN:string = this.HTML_ROOT + "main";
+    HTML_TAB:string = this.HTML_ROOT + "tab";
 }
--- app/src/ts/main.ts
+++ app/src/ts/main.ts
@@ -1,15 +1,16 @@
-//import PathConst from "./constant/path/pathConst";
+import PathConst from "./constant/path/pathConst";
 
 function doGet(e:any) {
     console.log(e);
-    const html:any = HtmlService.createTemplateFromFile(PathConst.HTML_MAIN);
+    const pathConst:PathConst = new PathConst();
+    const html:any = HtmlService.createTemplateFromFile(pathConst.HTML_MAIN);
     return html
         .evaluate()
         .setTitle('moneyrecorder')
         .addMetaTag("viewport", "width=device-width, initial-scale=1");
 }
 
-// declare const global: {
-//     [x: string]: unknown;
-// };
+declare const global: {
+    [x: string]: unknown;
+};
 global.doGet = doGet;
